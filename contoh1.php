c36c4f3ad7afb4d6a824609587921dc701513f24
WP2-ZahroilBatul <zahroilbatul114@gmail.com>
2021-09-30 19:27:59 +0700

    Upload awal project

---
+++ contoh1.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 
+ */
+class contoh1 extends CI_Controller
+{
+	
+	public function index()
+	{
+		echo "<h1>Perkenalan</h1>";
+		echo "Nama saya Zahroil Batul<br>
+		Saya tinggal di daerah kebumen<br>
+		Olahraga yang saya sukai adalah volly";
+	}
+}
