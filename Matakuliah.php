8b788f640750f7f32cfc3b074ba6bee02207263f
WP2-ZahroilBatul <zahroilbatul114@gmail.com>
2021-10-17 19:52:23 +0700

    push

--- Matakuliah.php
+++ Matakuliah.php
@@ -7,11 +7,33 @@ class Matakuliah extends CI_Controller
 	}
 	public function cetak()
 	{
-		$data = [
-			'kode' => $this->input->post('kode'),
-			'nama' => $this->input->post('nama'),
-			'sks' => $this->input->post('sks')
-		];
-		$this->load->view('view-data-matakuliah', $data);
+		$this->form_validation->set_rules('kode', 'Kode', 'required',
+			array(
+				'required' => '%s Wajib diisi.'
+		));
+		$this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required',
+			array(
+				'required' => '%s Wajib diisi.'
+		));
+		$this->form_validation->set_rules('sks', 'SKS', 'required',
+			array(
+				'required' => '%s Wajib diisi.'
+		));
+
+
+
+
+		if ($this->form_validation->run() == FALSE) {
+
+			$this->load->view('view-form-matakuliah');
+		
+		} else {
+			$data = [
+				'kode' => $this->input->post('kode'),
+				'nama' => $this->input->post('nama'),
+				'sks' => $this->input->post('sks')
+			];
+			$this->load->view('view-data-matakuliah', $data);
+		}
 	}
 }
